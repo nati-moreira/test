373a761b2708b413886e865dfff2ebb82f7ea770
Natalia <nati_2205@hotmail.com>
2024-02-14 13:40:41 -0300

    atualizado app

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.header('Jogando uma moeda')
 
-st.write('Ainda não é um aplicativo funcional. Em construção.')
+number_of_trails = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
 
-print(ola)
+if start_button:
+    st.write(f'Executandoo o experimento de {number_of_trails} tentativas.')
+
+st.write('Ainda não é um aplicativo funcional. Em construção')
